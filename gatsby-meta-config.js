a3fff342667d8760876b4ac2b992227a806e3f55
Donghoon Song <thdehdgns@gmail.com>
2020-01-02 18:24:45 +0900

    Initial commit

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  title: `사진찍는 웹 개발자`,
+  description: `React!`,
+  author: `Donghoon Song`,
+  introduction: `React!!`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    github: `Donghoon759`, // Your GitHub account
+    medium: `hoon_donghoon`, // Your Medium account
+    facebook: `thdehdgns759`, // Your Facebook account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
